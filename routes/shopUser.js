01a576f016133351e4426fc943e039e0aa889780
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-05-25 13:18:18 +0200

    shop app node js

---
+++ routes/shopUser.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+
+const adminData = require('./admin')
+
+const router = express.Router()
+
+router.get("/", (req, res, next) => {
+    const products = adminData.products
+    res.render('shop', { prods: products, pageTitle: 'tota shop', path: "/"})
+})
+
+module.exports = router
